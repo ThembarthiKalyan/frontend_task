7efa3def79044d1cd619082c4f18fc5485254b34
ThembarthiKalyan <88373881+ThembarthiKalyan@users.noreply.github.com>
2024-08-12 11:22:33 +0530

    Create Dashboard.js

---
+++ login-app/login-frontend/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,7 @@
+function Dashboard(){
+    return(
+        <div style={{textAlign: "center", color: "blue"}}>Welcome to Dashboard page. User Verified Successfully</div>
+    )
+}
+
+export default Dashboard;
